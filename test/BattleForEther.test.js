7e4de7a03a0630758d553ddb48aafe1d73c03274
Sylvain P <sylvain@pop-os.localdomain>
2020-05-06 22:25:00 +0200

    wip

---
+++ test/BattleForEther.test.js
@@ -0,0 +1,13 @@
+const BattleForEther = artifacts.require("./BattleForEther.sol");
+
+contract("BattleForEther", () => {
+  describe("consctructor", () => {
+    it("should store its parameters", async () => {});
+  });
+  describe("join()", () => {
+    it("should not able to join defore the game stats", () => {});
+    it("should not able to join after it gas ender", () => {});
+    it("should get some unites int the begining", () => {});
+    it("should emit a newPlayer event", () => {});
+  });
+});
