662f06357a13187944b44a948679d94fbeb6e0b3
Sylvain P <sylvain@pop-os.localdomain>
2020-05-08 09:16:49 +0200

    8

--- test/BattleForEther.test.js
+++ test/BattleForEther.test.js
@@ -1,35 +1,49 @@
-const BattleForEther = artifacts.require("./BattleForEther.sol");
+const BattleForEther = artifacts.require('./BattleForEther.sol')
 
-contract("BattleForEther", () => {
-  describe("consctructor", () => {
-    it("should store its parameters", async () => {
-      const startDate = ~~(Date.now() / 1000);
-      const endDate = ~~(Date.now() / 1000) + 60 * 60;
-      const instance = await BattleForEther.new(startDate, endDate);
+contract('BattleForEther', () => {
+  describe('consctructor', () => {
+    it('should store its parameters', async () => {
+      const startDate = ~~(Date.now() / 1000)
+      const endDate = ~~(Date.now() / 1000) + 60 * 60
+      const instance = await BattleForEther.new(startDate, endDate)
 
-      assert.equal(await instance.startDate(), startDate, "wrong startDate");
-      assert.equal(await instance.endDate(), endDate, "wrong endDate");
+      assert.equal(await instance.startDate(), startDate, 'wrong startDate')
+      assert.equal(await instance.endDate(), endDate, 'wrong endDate')
       //console.log(instance)
-    });
-  });
-  describe("join()", () => {
-    it("should not able to join defore the game stats", async () => {
-      const startDate = ~~(Date.now() / 1000) + 2 * 60;
-      const endDate = ~~(Date.now() / 1000) + 60 * 60;
-      const instance = await BattleForEther.new(startDate, endDate);
+    })
+  })
+  describe('join()', () => {
+    it('should not able to join defore the game stats', async () => {
+      const startDate = ~~(Date.now() / 1000) + 2 * 60
+      const endDate = ~~(Date.now() / 1000) + 60 * 60
+      const instance = await BattleForEther.new(startDate, endDate)
 
-      let hasFailed = false;
+      let hasFailed = false
 
       try {
-        await instance.join();
+        await instance.join()
       } catch (error) {
-        hasFailed = true;
+        hasFailed = true
       }
 
-      assert.equal(hasFailed, true, "player was able to join before start");
-    });
-    it("should not able to join after it gas ender", () => {});
-    it("should get some unites int the begining", () => {});
-    it("should emit a newPlayer event", () => {});
-  });
-});
+      assert.equal(hasFailed, true, 'player was able to join before start')
+    })
+    it('should not able to join after it gas ender', async () => {
+      const startDate = ~~(Date.now() / 1000) - 60 * 60
+      const endDate = ~~(Date.now() / 1000) + 60 * 60
+      const instance = await BattleForEther.new(startDate, endDate)
+
+      let hasFailed = false
+
+      try {
+        await instance.join()
+      } catch (error) {
+        hasFailed = true
+      }
+
+      assert.equal(hasFailed, true, 'player was able to join after start')
+    })
+    it('should get some unites int the begining', () => {})
+    it('should emit a newPlayer event', () => {})
+  })
+})
